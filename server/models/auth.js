67ba8e13cabfd0005c2f994dc97d9f17a4183f55
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-25 22:56:19 +0200

    added auth.js MCR

---
+++ server/models/auth.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+
+const AuthSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+})
