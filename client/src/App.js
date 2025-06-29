c9884a5c4a3e2efb542510dba93753eb7def68cf
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-29 22:58:47 +0200

    added Modal.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,19 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import useToken from './hooks/useToken';
 import Navbar from './components/Navbar';
+import Modal from './components/Modal';
 
 function App() {
 
     const [token] = useToken()
 
-    console.log(token.token, "token");
-
     return (
         <div>
             <BrowserRouter>
-                {token.token && <Navbar />}
+                {token?.token && <Navbar />}
+                <Modal />
                 <Routes>
-                    <Route path="/" element={!token.token ? <Link to={'/auth'} /> : <Home />} />
+                    <Route path="/" element={!token?.token ? <Link to={'/auth'} /> : <Home />} />
                     <Route path="/auth" element={<Auth />} />
                 </Routes>
             </BrowserRouter>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { BiLogOut } from 'react-icons/bi'
 
 const Navbar = () => {
+
+    const logoutFunc = () => {
+        localStorage.clear()
+        window.location = '/auth'
+    }
+
     return (
         <div className='h-20 bg-indigo-600 flex items-center justify-between px-5'>
             <div className='text-white font-bold text-2xl cursor-pointer'>
@@ -12,7 +18,7 @@ const Navbar = () => {
                 <div className='w-36 border border-indigo-900 p-2 rounded-md text-center text-white cursor-pointer'>
                     Post Create
                 </div>
-                <BiLogOut size={25} className='text-white cursor-pointer' />
+                <BiLogOut onClick={logoutFunc} size={25} className='text-white cursor-pointer' />
             </div>
         </div>
     )
